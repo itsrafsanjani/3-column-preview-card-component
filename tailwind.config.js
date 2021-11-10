2eadb95c45a839a5c85b2935c52c079607e15980
itsrafsanjani <mdrafsanjanirafin@gmail.com>
2021-11-10 22:16:54 +0600

    feat: 3 column preview card component done

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors: {
+        brightOrange: 'hsl(31, 77%, 52%)',
+        darkCyan: 'hsl(184, 100%, 22%)',
+        veryDarkCyan: 'hsl(179, 100%, 13%)',
+        transparentWhite: 'hsla(0, 0%, 100%, 0.75)',
+        veryLightGray: 'hsl(0, 0%, 95%)',
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
